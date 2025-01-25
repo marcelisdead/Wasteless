3122f49c13e7b81fe3158a0805ece4eaaa73b131
Marcel <46794418+marcelisdead@users.noreply.github.com>
2025-01-25 01:40:56 -0500

    Machine progresses through items. need to check in vr

--- Assets/_Assets/Marcel/Scripts/Scannable.cs
+++ Assets/_Assets/Marcel/Scripts/Scannable.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Scannable : MonoBehaviour
 {
+    public bool isScanned;
 
+    public UnityEvent EventOnScan;
+    public UnityEvent EventOnBag;
 
     // Start is called before the first frame update
     void Start()
@@ -12,9 +16,24 @@ public class Scannable : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (!isScanned)
+        {
+            if(other == ScanningMachine.Instance.scanCollider)
+            {
+                isScanned = true;
+                EventOnScan.Invoke();
+                ScanningMachine.Instance.Scan();
+            }
+        }
+        else
+        {
+            if(other == ScanningMachine.Instance.bagCollider)
+            {
+                EventOnBag.Invoke();
+                ScanningMachine.Instance.Bag();
+            }
+        }
     }
 }
--- Assets/_Assets/Marcel/Scripts/ScanningMachine.cs
+++ Assets/_Assets/Marcel/Scripts/ScanningMachine.cs
@@ -1,19 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ScanningMachine : MonoBehaviour
 {
-    
+    public Collider scanCollider, bagCollider;
+    public Transform startPos, respawnPos;
+
+    public Scannable[] scannables;
+    public Transform[] presentables;
+
+    public int currentScannable = 0;
+
+    public UnityEvent EventOnScan;
+    public UnityEvent EventOnBag;
+
+    public static ScanningMachine Instance;
+    private void Awake()
+    {
+     if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+     foreach(var s in scannables)
+        {
+            s.gameObject.SetActive(false);
+        }
+        scannables[0].transform.position = startPos.position;
+        scannables[0].gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NextItem()
     {
-        
+        scannables[currentScannable].gameObject.SetActive(false);
+        currentScannable++;
+
+        scannables[currentScannable].transform.position = startPos.position;
+        scannables[currentScannable].gameObject.SetActive(true);
+    }
+
+    public void Scan()
+    {
+        //presentables[currentScannable].gameObject.SetActive(true);
+        EventOnScan.Invoke();
+    }
+
+    public void Bag()
+    {
+        EventOnBag.Invoke();
+        NextItem();
     }
 }
